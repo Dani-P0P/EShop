e049f5df52a41e1ff342730a18b118b38e54479f
Dani_Pop <danielflorentin.pop@gmail.com>
2022-08-29 23:02:38 +0300

    Added Featured Products (For The Home Page)

--- EShop/Shared/Product.cs
+++ EShop/Shared/Product.cs
@@ -18,6 +18,8 @@ namespace EShop.Shared
 
         public int CategoryId { get; set; }
 
+        public bool Featured { get; set; } = false;
+
         public List<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
 
     }
