da8b18ede606d7f111aba0857004246701e1cdc9
Dani_Pop <danielflorentin.pop@gmail.com>
2022-08-29 20:27:12 +0300

    Add project files.

---
+++ EShop/Client/Program.cs
@@ -0,0 +1,18 @@
+global using EShop.Shared;
+global using System.Net.Http.Json;
+global using EShop.Client.Services.ProductService;
+global using EShop.Client.Services.CategoryService;
+using EShop.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+await builder.Build().RunAsync();
---
+++ EShop/Server/Services/ProductService/IProductService.cs
@@ -0,0 +1,11 @@
+﻿namespace EShop.Server.Services.ProductService
+{
+    public interface IProductService
+    {
+        Task<ServiceResponse<List<Product>>> GetProductsAsync();
+        Task<ServiceResponse<Product>> GetProductAsync(int productId);
+        Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl);
+        Task<ServiceResponse<List<Product>>> SearchProducts(string searchText);
+        Task<ServiceResponse<List<string>>> GetProductSearchSuggestions(string searchText); 
+    }
+}
