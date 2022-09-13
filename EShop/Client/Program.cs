d1ee3aec97076fc6e147644f2bbe12d82145e075
Dani_Pop <danielflorentin.pop@gmail.com>
2022-09-13 14:36:55 +0300

    Added Local Storage, Cart Counter, CartItem Class, CartService on the Client, "AddToCart" Button. Show the number of items in the Cart Counter.

--- EShop/Client/Program.cs
+++ EShop/Client/Program.cs
@@ -2,17 +2,20 @@ global using EShop.Shared;
 global using System.Net.Http.Json;
 global using EShop.Client.Services.ProductService;
 global using EShop.Client.Services.CategoryService;
+global using EShop.Client.Services.CartService;
 using EShop.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICartService, CartService>();
 
 await builder.Build().RunAsync();
