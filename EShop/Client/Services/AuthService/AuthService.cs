38557b417fb36c5a74202c8af4bfcae813b95b1d
Dani_Pop <danielflorentin.pop@gmail.com>
2022-09-19 22:41:24 +0300

    Implemented the Login on the Client Side

--- EShop/Client/Services/AuthService/AuthService.cs
+++ EShop/Client/Services/AuthService/AuthService.cs
@@ -8,6 +8,13 @@
         {
             _http = http;
         }
+
+        public async Task<ServiceResponse<string>> Login(UserLoginDTO request)
+        {
+            var result = await _http.PostAsJsonAsync("api/auth/login", request);
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<string>>();
+        }
+
         public async Task<ServiceResponse<int>> Register(UserRegisterDTO request)
         {
             var result = await _http.PostAsJsonAsync("api/auth/register", request);
