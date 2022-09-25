833a7e4dfac767e1669b176226c4d4f24d759cd8
Dani_Pop <danielflorentin.pop@gmail.com>
2022-09-25 21:36:43 +0300

    Implemented Custom Authentication State Provider Exposed Authentication State Added Logout Option with AuthorizeView Component

--- EShop/Client/Program.cs
+++ EShop/Client/Program.cs
@@ -4,6 +4,7 @@ global using EShop.Client.Services.ProductService;
 global using EShop.Client.Services.CategoryService;
 global using EShop.Client.Services.CartService;
 global using EShop.Client.Services.AuthService;
+global using Microsoft.AspNetCore.Components.Authorization;
 using EShop.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -19,5 +20,8 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 await builder.Build().RunAsync();
